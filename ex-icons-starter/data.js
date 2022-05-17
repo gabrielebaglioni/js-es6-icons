f6d763175402f005eb395c79a1135a2b42231b60
gabrielebaglioni <gabrielebaglioni55@gmail.com>
2022-05-17 17:24:41 +0200

    logica non funzionante

--- ex-icons-starter/data.js
+++ ex-icons-starter/data.js
@@ -112,17 +112,30 @@ const icons = [{
     }
 ];
 
-const conatinerHTML = document.querySelector("container");
-const boxIcon = document.querySelector("#tpl-box-icon").content;
+// const conatinerHTML = document.querySelector("container");
+// const boxIcon = document.querySelector("#tpl-box-icon").content;
 
-function boxIconHTML(print, elm) {
-    let card = boxIcon.cloneNode(true);
-    card.querySelector("i").classList.add(elm.family, elm.prefix + elm.name, elm.color);
-    card.querySelector("span").innerHTML = elm.name;
-    print.append(card);
+// function boxIconHTML(print, elm) {
+//     let card = boxIcon.cloneNode(true);
+//     card.querySelector("i").classList.add(elm.family, elm.prefix + elm.name, elm.color);
+//     card.querySelector("span").innerHTML = elm.name;
+//     print.append(card);
+// }
+// //creo uja funzione per stampare la funzione precedente per tutti gli ogetti
+// function printIconHTML(print, elm) {
+//     elm.forEach(element => boxIconHTML(print, element));
+// }
+// printIconHTML(conatinerHTML, icon)
+const Container = document.querySelector(".container");
+//ciclo per la lunghezza del mio ogetto
+for (let i = 0; i < icons.length; i++) {
+    const element = icons[i]
+    const printIcon = `
+	<div class="box-icon">
+                 <i class="fa-solid fa-${element.name}" style = "color: ${element.color}></i>
+                 <span>${element.name}</span>
+   </div>
+	`
+    Container.innerHTML += printIcon;
+
 }
-//creo uja funzione per stampare la funzione precedente per tutti gli ogetti
-function printIconHTML(print, elm) {
-    elm.forEach(element => boxIconHTML(print, element));
-}
-printIconHTML(conatinerHTML, icons);
